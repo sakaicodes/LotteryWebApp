eab3797f0ae579e958339f208d42a032b701b6cb
sakainewman <c1032319@newcastle.ac.uk>
2022-11-22 21:01:13 +0000

    moved lottery key to draws table

--- models.py
+++ models.py
@@ -6,6 +6,14 @@ from app import db, app
 import bcrypt
 
 
+def encrypt(data, numbers):
+    return Fernet(numbers).encrypt(bytes(data, 'utf-8'))
+
+
+def decrypt(data, numbers):
+    return Fernet(numbers).decrypt(data).decode('utf-8')
+
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
@@ -20,7 +28,6 @@ class User(db.Model, UserMixin):
     lastname = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(100), nullable=False, default='user')
-    lotterykey = db.Column(db.BLOB)
 
     # Define the relationship to Draw
     draws = db.relationship('Draw')
@@ -32,7 +39,7 @@ class User(db.Model, UserMixin):
         self.phone = phone
         self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         self.role = role
-        self.lotterykey = Fernet.generate_key()
+
 
 
 class Draw(db.Model):
@@ -58,6 +65,9 @@ class Draw(db.Model):
     # Lottery round that draw is used
     lottery_round = db.Column(db.Integer, nullable=False, default=0)
 
+    # Cryptography
+    lottery_key = db.Column(db.BLOB)
+
     def __init__(self, user_id, numbers, master_draw, lottery_round):
         self.user_id = user_id
         self.numbers = numbers
@@ -65,6 +75,7 @@ class Draw(db.Model):
         self.matches_master = False
         self.master_draw = master_draw
         self.lottery_round = lottery_round
+        self.lottery_key = Fernet.generate_key()
 
 
 def init_db():
